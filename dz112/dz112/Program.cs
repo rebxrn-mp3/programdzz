181103abf125ed51dca6ec78ee3131f2a2b18bdc
tfeda <tfeda2005@icloud.com>
2024-12-16 16:38:06 +0500

    3

---
+++ dz112/dz112/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Write("Введите размер массива: ");
+        int size = int.Parse(Console.ReadLine());
+
+        int[] array = new int[size];
+
+        Console.WriteLine("Введите элементы массива:");
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write($"Элемент {i + 1}: ");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Исходный массив: " + string.Join(", ", array));
+
+        ModifyArray(array);
+
+        Console.WriteLine("Измененный массив: " + string.Join(", ", array));
+        Console.ReadKey();
+    }
+
+    static void ModifyArray(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int sum = 0;
+            for (int j = 0; j <= i; j++)
+            {
+                sum += arr[j];
+            }
+            arr[i] = sum;
+        }
+    }
+}
---
+++ dz114/dz114/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите размер массива: ");
+        int size = int.Parse(Console.ReadLine());
+
+        int[] array = new int[size];
+
+        Console.WriteLine("Введите элементы массива:");
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write($"Элемент {i + 1}: ");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.Write("Введите число л (лямбда): ");
+        double lambda = double.Parse(Console.ReadLine());
+
+        double[] differences = CalculateDifferences(array, lambda);
+
+        Console.WriteLine("Массив разностей:");
+        Console.WriteLine(string.Join(", ", differences));
+        Console.ReadKey();
+    }
+
+    static double[] CalculateDifferences(int[] arr, double a)
+    {
+        double[] differences = new double[arr.Length];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            differences[i] = arr[i] - a;
+        }
+
+        return differences;
+    }
+}
